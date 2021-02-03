c0e6ac70dc5f35c3c578e718c79d4bd8b5a2a51f
Vitor Arjol <vitor@ecorban.com.br>
2021-02-03 09:08:06 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('manifest', json_decode(file_get_contents(public_path('manifest.json')), true));
+        if (is_file(public_path('manifest.json'))) {
+            View::share('manifest', json_decode(file_get_contents(public_path('manifest.json')), true));
+        } else {
+            View::share('manifest', [], true);
+        }
     }
 }
